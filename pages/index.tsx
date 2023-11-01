a43f4c5c37cb616e69bdee190a512f16e2eccdce
MakhmutT <97080565+MakhmutT@users.noreply.github.com>
2023-11-01 23:24:54 +0600

    add layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next';
-import { Button, Htag, P, Tag } from '../components';
+import { Button, Htag, P, Rating, Tag } from '../components';
+import { useState } from 'react';
+import { withLayout } from '../layout/Layout';
 
 const Home: NextPage = () => {
+  const [rating, setRating] = useState<number>(4);
   return (
     <>
-      <Htag tag='h3'>Text</Htag>
-      <Button appearance='primary' arrow="down">Кнопка</Button>
+      <Htag tag='h3'>asd</Htag>
+      <Button appearance='primary'>Кнопка</Button>
       <Button appearance='ghost' arrow="down">Кнопка</Button>
       <P size="S">Маленький</P>
       <P>Средний</P>
@@ -14,8 +17,9 @@ const Home: NextPage = () => {
       <Tag size="M" color="red">Red</Tag>
       <Tag size="S" color="green">green</Tag>
       <Tag color="primary">primary</Tag>
+      <Rating rating={rating} isEditable={true} setRating={setRating} />
     </>
   );
 };
 
-export default Home;
+export default withLayout(Home);
